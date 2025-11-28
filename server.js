f58dd2d32cf15ab9bf4477616060828d50d3407f
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-11-28 23:37:31 +0500

    Import User model in server.js for user-related operations

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import session from "express-session"
 
+import { User } from "./models/user.js"
 import passport from "./config/passport.js";
 import { auth } from "./routes/auth.js";
 
