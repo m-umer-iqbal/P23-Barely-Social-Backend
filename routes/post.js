e31b4dbcadf6fe0bada340b79604201785926d00
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-12-18 00:16:46 +0500

    feat: include profile picture in posts fetching response

--- routes/post.js
+++ routes/post.js
@@ -55,14 +55,14 @@ router.get("/", async (req, res) => {
             const loggedInUserProfile = await User.findById(userId)
             const posts = await Post.find({
                 author: { $in: loggedInUserProfile.following }
-            }).populate("author", "fullname username");
+            }).populate("author", "fullname username profilePicture");
             res.status(200).json({
                 success: true,
                 message: "User's Posts Fetched Successfully",
                 posts: posts
             })
         } else {
-            const posts = await Post.find({ author: userId }).populate("author", "fullname username")
+            const posts = await Post.find({ author: userId }).populate("author", "fullname username profilePicture")
             res.status(200).json({
                 success: true,
                 message: "User's Posts Fetched Successfully",
